c4ce6f975622dc1cbae540ab7da1c7d5eebc4001
Alex Vandenberg <a.vandenberg0175@gmail.com>
2021-06-30 11:23:43 -0400

    Updating to current sample

--- hid-mouse/src/main.c
+++ hid-mouse/src/main.c
@@ -11,160 +11,71 @@
 #include <usb/usb_device.h>
 #include <usb/class/usb_hid.h>
 
-#include <math.h>
-
 #define LOG_LEVEL LOG_LEVEL_DBG
 LOG_MODULE_REGISTER(main);
 
+/*
+ * Devicetree node identifiers for the buttons and LED this sample
+ * supports.
+ */
 #define SW0_NODE DT_ALIAS(sw0)
-
-#if DT_NODE_HAS_STATUS(SW0_NODE, okay)
-#define PORT0		DT_GPIO_LABEL(SW0_NODE, gpios)
-#define PIN0		DT_GPIO_PIN(SW0_NODE, gpios)
-#define PIN0_FLAGS	DT_GPIO_FLAGS(SW0_NODE, gpios)
-#else
-#error "Unsupported board: sw0 devicetree alias is not defined"
-#define PORT0		""
-#define PIN0		0
-#define PIN0_FLAGS	0
-#endif
-
 #define SW1_NODE DT_ALIAS(sw1)
-
-#if DT_NODE_HAS_STATUS(SW1_NODE, okay)
-#define PORT1		DT_GPIO_LABEL(SW1_NODE, gpios)
-#define PIN1		DT_GPIO_PIN(SW1_NODE, gpios)
-#define PIN1_FLAGS	DT_GPIO_FLAGS(SW1_NODE, gpios)
-#endif
-
 #define SW2_NODE DT_ALIAS(sw2)
-
-#if DT_NODE_HAS_STATUS(SW2_NODE, okay)
-#define PORT2		DT_GPIO_LABEL(SW2_NODE, gpios)
-#define PIN2		DT_GPIO_PIN(SW2_NODE, gpios)
-#define PIN2_FLAGS	DT_GPIO_FLAGS(SW2_NODE, gpios)
-#endif
-
 #define SW3_NODE DT_ALIAS(sw3)
-
-#if DT_NODE_HAS_STATUS(SW3_NODE, okay)
-#define PORT3		DT_GPIO_LABEL(SW3_NODE, gpios)
-#define PIN3		DT_GPIO_PIN(SW3_NODE, gpios)
-#define PIN3_FLAGS	DT_GPIO_FLAGS(SW3_NODE, gpios)
-#endif
-
 #define LED0_NODE DT_ALIAS(led0)
 
-#if DT_NODE_HAS_STATUS(LED0_NODE, okay)
-#define LED_PORT	DT_GPIO_LABEL(LED0_NODE, gpios)
-#define LED		DT_GPIO_PIN(LED0_NODE, gpios)
-#define LED_FLAGS	DT_GPIO_FLAGS(LED0_NODE, gpios)
-#else
-#error "Unsupported board: led0 devicetree alias is not defined"
-#define LED_PORT	""
-#define LED		0
-#define LED_FLAGS	0
+/*
+ * Button sw0 and LED led0 are required.
+ */
+#if !DT_NODE_EXISTS(SW0_NODE)
+#error "Unsupported board: sw0 devicetree alias is not defined"
 #endif
 
-/* Example HID report descriptors */
-/**
- * @brief Simple HID mouse report descriptor for n button mouse.
- *
- * @param bcnt	Button count. Allowed values from 1 to 8.
+#if !DT_NODE_EXISTS(LED0_NODE)
+#error "Unsupported board: led0 devicetree alias is not defined"
+#endif
+
+/*
+ * Helper macro for initializing a gpio_dt_spec from the devicetree
+ * with fallback values when the nodes are missing.
  */
-#define HID_GAMEPAD_REPORT_DESC() {				\
-	/* USAGE_PAGE (Generic Desktop) */			\
-	HID_GI_USAGE_PAGE, USAGE_GEN_DESKTOP,			\
-	/* USAGE (Game Pad) */					\
-	HID_LI_USAGE, USAGE_GEN_DESKTOP_GAMEPAD,			\
-	/* COLLECTION (Application) */				\
-	HID_MI_COLLECTION, COLLECTION_APPLICATION,		\
-		/* COLLECTION (Physical) */			\
-		HID_MI_COLLECTION, COLLECTION_PHYSICAL,		\
-			/* Bits used for button signalling */	\
-			/* USAGE_PAGE (Button) */		\
-			HID_GI_USAGE_PAGE, USAGE_GEN_BUTTON,	\
-			/* USAGE_MINIMUM (Button 1) */		\
-			HID_LI_USAGE_MIN(1), 0x01,		\
-			/* USAGE_MAXIMUM (Button bcnt) */	\
-			HID_LI_USAGE_MAX(1), 0x08,		\
-			/* LOGICAL_MINIMUM (0) */		\
-			HID_GI_LOGICAL_MIN(1), 0x00,		\
-			/* LOGICAL_MAXIMUM (1) */		\
-			HID_GI_LOGICAL_MAX(1), 0x01,		\
-			/* REPORT_SIZE (1 bit) */			\
-			HID_GI_REPORT_SIZE, 0x01,		\
-			/* REPORT_COUNT (8 reports) */		\
-			HID_GI_REPORT_COUNT, 0x08,		\
-			/* INPUT (Data,Var,Abs) */		\
-			HID_MI_INPUT, 0x02,			\
-			/* X, Y, RX, RY */		\
-			/* USAGE_PAGE (Generic Desktop) */	\
-			HID_GI_USAGE_PAGE, USAGE_GEN_DESKTOP,	\
-			/* USAGE (X) */				\
-			HID_LI_USAGE, USAGE_GEN_DESKTOP_X,	\
-			/* USAGE (Y) */				\
-			HID_LI_USAGE, USAGE_GEN_DESKTOP_Y,	\
-			/* USAGE (RX) */				\
-			HID_LI_USAGE, 0x33,	\
-			/* USAGE (RY) */				\
-			HID_LI_USAGE, 0x34,	\
-			/* LOGICAL_MINIMUM (-127) */		\
-			HID_GI_LOGICAL_MIN(1), -127,		\
-			/* LOGICAL_MAXIMUM (127) */		\
-			HID_GI_LOGICAL_MAX(1), 127,		\
-			/* REPORT_SIZE (8) */			\
-			HID_GI_REPORT_SIZE, 0x08,		\
-			/* REPORT_COUNT (3) */			\
-			HID_GI_REPORT_COUNT, 0x04,		\
-			/* INPUT (Data,Var,Abs) */		\
-			HID_MI_INPUT, 0x02,			\
-		/* END_COLLECTION */				\
-		HID_MI_COLLECTION_END,				\
-	/* END_COLLECTION */					\
-	HID_MI_COLLECTION_END,					\
-}
+#define GPIO_SPEC(node_id) GPIO_DT_SPEC_GET_OR(node_id, gpios, {0})
 
-static const uint8_t hid_report_desc[] = HID_GAMEPAD_REPORT_DESC();
+/*
+ * Create gpio_dt_spec structures from the devicetree.
+ */
+static const struct gpio_dt_spec sw0 = GPIO_SPEC(SW0_NODE),
+	sw1 = GPIO_SPEC(SW1_NODE),
+	sw2 = GPIO_SPEC(SW2_NODE),
+	sw3 = GPIO_SPEC(SW3_NODE),
+	led0 = GPIO_SPEC(LED0_NODE);
 
-static uint8_t def_val[12];
-static volatile uint8_t status[5];
+static const uint8_t hid_report_desc[] = HID_MOUSE_REPORT_DESC(2);
+
+static uint8_t def_val[4];
+static volatile uint8_t status[4];
 static K_SEM_DEFINE(sem, 0, 1);	/* starts off "not available" */
-static struct gpio_callback callback[12];
+static struct gpio_callback callback[4];
 static enum usb_dc_status_code usb_status;
 
-#define GAMEPAD_BTN_REPORT_POS	0
-#define GAMEPAD_X_REPORT_POS	1
-#define GAMEPAD_Y_REPORT_POS	2
-#define GAMEPAD_RX_REPORT_POS	3
-#define GAMEPAD_RY_REPORT_POS	4
+#define MOUSE_BTN_REPORT_POS	0
+#define MOUSE_X_REPORT_POS	1
+#define MOUSE_Y_REPORT_POS	2
 
-#define GAMEPAD_BTN_ONE			BIT(0)
-#define GAMEPAD_BTN_TWO			BIT(1)
-#define GAMEPAD_BTN_THREE		BIT(2)
-#define GAMEPAD_BTN_FOUR		BIT(3)
-#define GAMEPAD_BTN_FIVE		BIT(4)
-#define GAMEPAD_BTN_SIX			BIT(5)
-#define GAMEPAD_BTN_SEVEN		BIT(6)
-#define GAMEPAD_BTN_EIGHT		BIT(7)
-
-
+#define MOUSE_BTN_LEFT		BIT(0)
+#define MOUSE_BTN_RIGHT		BIT(1)
+#define MOUSE_BTN_MIDDLE	BIT(2)
 
 static void status_cb(enum usb_dc_status_code status, const uint8_t *param)
 {
 	usb_status = status;
 }
 
-static void button_press(const struct device *gpio, struct gpio_callback *cb,
+static void left_button(const struct device *gpio, struct gpio_callback *cb,
 			uint32_t pins)
 {
-	// pins is BIT(pin) of the actual pin number. need to that log2 to get the
-	// actual pin number. This then needs to get translated to gamepad button
-	// number
-	LOG_INF("BUTTON PRESSED!!! %u, %u, %lu, %lu, %i", pins, cb->pin_mask,
-			GAMEPAD_BTN_ONE, (ulong_t)log2(pins), PIN0);
 	int ret;
-	uint8_t state = status[GAMEPAD_BTN_REPORT_POS];
+	uint8_t state = status[MOUSE_BTN_REPORT_POS];
 
 	if (IS_ENABLED(CONFIG_USB_DEVICE_REMOTE_WAKEUP)) {
 		if (usb_status == USB_DC_SUSPEND) {
@@ -173,80 +84,67 @@ static void button_press(const struct device *gpio, struct gpio_callback *cb,
 		}
 	}
 
-	// Ensure only 1 pin is in this callback
-	if (is_power_of_two(pins) == false) {
-		LOG_ERR("More than one pin in button callback at a time");
-	}
-
-	// Get the state of the button
-	// assumes only 1 pin is returned in the callback
-	// maybe need to put a check that pins is a clean power of 2
-	// or do something with the cb->pin_mask, but that only works if only 1 pin
-	// is on the callback
-	ret = gpio_pin_get(gpio, log2(pins));
+	ret = gpio_pin_get(gpio, sw0.pin);
 	if (ret < 0) {
-		LOG_ERR("Failed to get the state of pin %d, error: %d",
-			(int)log2(pins), ret);
+		LOG_ERR("Failed to get the state of port %s pin %u, error: %d",
+			gpio->name, sw0.pin, ret);
 		return;
 	}
-	LOG_INF("gpio_pin_get has value: %d", ret);
 
-	// Determine the button pressed
-	int btn = 0;
-	switch ((int)log2(pins)) {
-#if DT_NODE_HAS_STATUS(SW0_NODE, okay)
-		case PIN0: btn = GAMEPAD_BTN_ONE; break;
-#endif
-#if DT_NODE_HAS_STATUS(SW1_NODE, okay)
-		case PIN1: btn = GAMEPAD_BTN_TWO; break;
-#endif
-#if DT_NODE_HAS_STATUS(SW2_NODE, okay)
-		case PIN2: btn = GAMEPAD_BTN_THREE; break;
-#endif
-#if DT_NODE_HAS_STATUS(SW3_NODE, okay)
-		case GAMEPAD_BTN_FOUR: btn = GAMEPAD_BTN_FOUR; break;
-#endif
-#if DT_NODE_HAS_STATUS(SW4_NODE, okay)
-		case GAMEPAD_BTN_FIVE: btn = GAMEPAD_BTN_FIVE; break;
-#endif
-#if DT_NODE_HAS_STATUS(SW5_NODE, okay)
-		case GAMEPAD_BTN_SIX: btn = GAMEPAD_BTN_SIX; break;
-#endif
-#if DT_NODE_HAS_STATUS(SW6_NODE, okay)
-		case GAMEPAD_BTN_SEVEN: btn = GAMEPAD_BTN_SEVEN; break;
-#endif
-#if DT_NODE_HAS_STATUS(SW7_NODE, okay)
-		case GAMEPAD_BTN_EIGHT: btn = GAMEPAD_BTN_EIGHT; break;
-#endif
-		default: LOG_ERR("pins doesn't match any known pins"); break;
-	}
-
-	LOG_INF("Button %d callback has been called", btn);
-
-	// Set or reset the button state
-	if (ret == 0) {
-		state &= ~btn;
+	if (def_val[0] != (uint8_t)ret) {
+		state |= MOUSE_BTN_LEFT;
 	} else {
-		state |= btn;
+		state &= ~MOUSE_BTN_LEFT;
 	}
 
-	if (status[GAMEPAD_BTN_REPORT_POS] != state) {
-		status[GAMEPAD_BTN_REPORT_POS] = state;
+	if (status[MOUSE_BTN_REPORT_POS] != state) {
+		status[MOUSE_BTN_REPORT_POS] = state;
 		k_sem_give(&sem);
 	}
 }
 
-/*
+static void right_button(const struct device *gpio, struct gpio_callback *cb,
+			 uint32_t pins)
+{
+	int ret;
+	uint8_t state = status[MOUSE_BTN_REPORT_POS];
+
+	if (IS_ENABLED(CONFIG_USB_DEVICE_REMOTE_WAKEUP)) {
+		if (usb_status == USB_DC_SUSPEND) {
+			usb_wakeup_request();
+			return;
+		}
+	}
+
+	ret = gpio_pin_get(gpio, sw1.pin);
+	if (ret < 0) {
+		LOG_ERR("Failed to get the state of port %s pin %u, error: %d",
+			gpio->name, sw1.pin, ret);
+		return;
+	}
+
+	if (def_val[1] != (uint8_t)ret) {
+		state |= MOUSE_BTN_RIGHT;
+	} else {
+		state &= ~MOUSE_BTN_RIGHT;
+	}
+
+	if (status[MOUSE_BTN_REPORT_POS] != state) {
+		status[MOUSE_BTN_REPORT_POS] = state;
+		k_sem_give(&sem);
+	}
+}
+
 static void x_move(const struct device *gpio, struct gpio_callback *cb,
 		   uint32_t pins)
 {
 	int ret;
-	uint8_t state = status[GAMEPAD_X_REPORT_POS];
+	uint8_t state = status[MOUSE_X_REPORT_POS];
 
-	ret = gpio_pin_get(gpio, PIN2);
+	ret = gpio_pin_get(gpio, sw2.pin);
 	if (ret < 0) {
-		LOG_ERR("Failed to get the state of pin %u, error: %d",
-			PIN2, ret);
+		LOG_ERR("Failed to get the state of port %s pin %u, error: %d",
+			gpio->name, sw2.pin, ret);
 		return;
 	}
 
@@ -254,35 +152,64 @@ static void x_move(const struct device *gpio, struct gpio_callback *cb,
 		state += 10U;
 	}
 
-	if (status[GAMEPAD_X_REPORT_POS] != state) {
-		status[GAMEPAD_X_REPORT_POS] = state;
+	if (status[MOUSE_X_REPORT_POS] != state) {
+		status[MOUSE_X_REPORT_POS] = state;
 		k_sem_give(&sem);
 	}
 }
-*/
 
-int callbacks_configure(const struct device *gpio, uint32_t pin, int flags,
+static void y_move(const struct device *gpio, struct gpio_callback *cb,
+		   uint32_t pins)
+{
+	int ret;
+	uint8_t state = status[MOUSE_Y_REPORT_POS];
+
+	ret = gpio_pin_get(gpio, sw3.pin);
+	if (ret < 0) {
+		LOG_ERR("Failed to get the state of port %s pin %u, error: %d",
+			gpio->name, sw3.pin, ret);
+		return;
+	}
+
+	if (def_val[3] != (uint8_t)ret) {
+		state += 10U;
+	}
+
+	if (status[MOUSE_Y_REPORT_POS] != state) {
+		status[MOUSE_Y_REPORT_POS] = state;
+		k_sem_give(&sem);
+	}
+}
+
+int callbacks_configure(const struct gpio_dt_spec *spec,
 			gpio_callback_handler_t handler,
 			struct gpio_callback *callback, uint8_t *val)
 {
+	const struct device *gpio = spec->port;
+	gpio_pin_t pin = spec->pin;
 	int ret;
 
-	if (!gpio) {
-		LOG_ERR("Could not find PORT");
-		return -ENXIO;
+	if (gpio == NULL) {
+		/* Optional GPIO is missing. */
+		return 0;
 	}
 
-	ret = gpio_pin_configure(gpio, pin, GPIO_INPUT | flags);
+	if (!device_is_ready(gpio)) {
+		LOG_ERR("GPIO port %s is not ready", gpio->name);
+		return -ENODEV;
+	}
+
+	ret = gpio_pin_configure_dt(spec, GPIO_INPUT);
 	if (ret < 0) {
-		LOG_ERR("Failed to configure pin %u, error: %d",
-			pin, ret);
+		LOG_ERR("Failed to configure port %s pin %u, error: %d",
+			gpio->name, pin, ret);
 		return ret;
 	}
 
 	ret = gpio_pin_get(gpio, pin);
 	if (ret < 0) {
-		LOG_ERR("Failed to get the state of pin %u, error: %d",
-			pin, ret);
+		LOG_ERR("Failed to get the state of port %s pin %u, error: %d",
+			gpio->name, pin, ret);
 		return ret;
 	}
 
@@ -291,15 +218,17 @@ int callbacks_configure(const struct device *gpio, uint32_t pin, int flags,
 	gpio_init_callback(callback, handler, BIT(pin));
 	ret = gpio_add_callback(gpio, callback);
 	if (ret < 0) {
-		LOG_ERR("Failed to add the callback for pin %u, error: %d",
-			pin, ret);
+		LOG_ERR("Failed to add the callback for port %s pin %u, "
+			"error: %d",
+			gpio->name, pin, ret);
 		return ret;
 	}
 
-	ret = gpio_pin_interrupt_configure(gpio, pin, GPIO_INT_EDGE_BOTH);
+	ret = gpio_pin_interrupt_configure_dt(spec, GPIO_INT_EDGE_BOTH);
 	if (ret < 0) {
-		LOG_ERR("Failed to configure interrupt for pin %u, error: %d",
-			pin, ret);
+		LOG_ERR("Failed to configure interrupt for port %s pin %u, "
+			"error: %d",
+			gpio->name, pin, ret);
 		return ret;
 	}
 
@@ -310,11 +239,10 @@ void main(void)
 {
 	int ret;
 	uint8_t report[4] = { 0x00 };
-	const struct device *led_dev, *hid_dev;
+	const struct device *hid_dev;
 
-	led_dev = device_get_binding(LED_PORT);
-	if (led_dev == NULL) {
-		LOG_ERR("Cannot get LED");
+	if (!device_is_ready(led0.port)) {
+		LOG_ERR("LED device %s is not ready", led0.port->name);
 		return;
 	}
 
@@ -324,43 +252,33 @@ void main(void)
 		return;
 	}
 
-	ret = gpio_pin_configure(led_dev, LED, GPIO_OUTPUT | LED_FLAGS);
+	ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT);
 	if (ret < 0) {
 		LOG_ERR("Failed to configure the LED pin, error: %d", ret);
 		return;
 	}
 
-	if (callbacks_configure(device_get_binding(PORT0), PIN0, PIN0_FLAGS,
-				&button_press, &callback[0], &def_val[0])) {
+	if (callbacks_configure(&sw0, &left_button, &callback[0],
+				&def_val[0])) {
 		LOG_ERR("Failed configuring left button callback.");
 		return;
 	}
 
-#if DT_NODE_HAS_STATUS(SW1_NODE, okay)
-	if (callbacks_configure(device_get_binding(PORT1), PIN1, PIN1_FLAGS,
-				&button_press, &callback[1], &def_val[1])) {
+	if (callbacks_configure(&sw1, &right_button, &callback[1],
+				&def_val[1])) {
 		LOG_ERR("Failed configuring right button callback.");
 		return;
 	}
-#endif
 
-/*
-#if DT_NODE_HAS_STATUS(SW2_NODE, okay)
-	if (callbacks_configure(device_get_binding(PORT2), PIN2, PIN2_FLAGS,
-				&x_move, &callback[2], &def_val[2])) {
+	if (callbacks_configure(&sw2, &x_move, &callback[2], &def_val[2])) {
 		LOG_ERR("Failed configuring X axis movement callback.");
 		return;
 	}
-#endif
 
-#if DT_NODE_HAS_STATUS(SW3_NODE, okay)
-	if (callbacks_configure(device_get_binding(PORT3), PIN3, PIN3_FLAGS,
-				&y_move, &callback[3], &def_val[3])) {
+	if (callbacks_configure(&sw3, &y_move, &callback[3], &def_val[3])) {
 		LOG_ERR("Failed configuring Y axis movement callback.");
 		return;
 	}
-#endif
-*/
 
 	usb_hid_register_device(hid_dev,
 				hid_report_desc, sizeof(hid_report_desc),
@@ -377,21 +295,18 @@ void main(void)
 	while (true) {
 		k_sem_take(&sem, K_FOREVER);
 
-		report[GAMEPAD_BTN_REPORT_POS] = status[GAMEPAD_BTN_REPORT_POS];
-/*
 		report[MOUSE_BTN_REPORT_POS] = status[MOUSE_BTN_REPORT_POS];
 		report[MOUSE_X_REPORT_POS] = status[MOUSE_X_REPORT_POS];
 		status[MOUSE_X_REPORT_POS] = 0U;
 		report[MOUSE_Y_REPORT_POS] = status[MOUSE_Y_REPORT_POS];
 		status[MOUSE_Y_REPORT_POS] = 0U;
-*/
 		ret = hid_int_ep_write(hid_dev, report, sizeof(report), NULL);
 		if (ret) {
 			LOG_ERR("HID write error, %d", ret);
 		}
 
 		/* Toggle LED on sent report */
-		ret = gpio_pin_toggle(led_dev, LED);
+		ret = gpio_pin_toggle(led0.port, led0.pin);
 		if (ret < 0) {
 			LOG_ERR("Failed to toggle the LED pin, error: %d", ret);
 		}
